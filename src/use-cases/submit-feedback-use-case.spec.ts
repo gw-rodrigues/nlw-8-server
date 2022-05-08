3d666be60ffb788f742ed6f6b7f8044867783333
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-08 23:30:51 +0200

    Add test to jest

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,49 @@
-test("sum 2 + 2", ()=>{
-    expect(2+2).toBe(4)
+import { SubmitFeedbackUseCase } from "./submit-feedbacks-use-case"
+
+// spies = espiões - saber se uma função foi chamada (create ou sendMail)
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy }
+)
+
+describe('Submit feedback', ()=>{
+
+    it('should be able to submit a feedback', async()=>{
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,TEST'
+        })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalled()
+        expect(sendMailSpy).toHaveBeenCalled()
+    });
+
+    it('should not be able to submit without type', async()=>{
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64,TEST'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit without comment', async()=>{
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64,TEST'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit without an invalid screenshot', async()=>{
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'test.jpg'
+        })).rejects.toThrow();
+    });
+
 })
