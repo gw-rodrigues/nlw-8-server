10141374fe15ae544e0c962ef2ee2f8c5fac72af
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-05-11 12:27:53 +0200

    fixing mail image size

--- src/use-cases/submit-feedbacks-use-case.ts
+++ src/use-cases/submit-feedbacks-use-case.ts
@@ -40,7 +40,7 @@ export class SubmitFeedbackUseCase {
             `<div style="font-family:sans-serif; font-size:16px; color:#111;">`,
             `<p>Tipo do feedback: ${type}</p>`,
             `<p>Cometário: ${comment}</p>`,
-            `<img width="200" height="auto" src="${screenshot}"/>`,
+            `<img width="400" height="auto" src="${screenshot}"/>`,
             `</div>`
         ].join("\n")
     })
